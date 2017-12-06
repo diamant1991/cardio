91978c752d6151c07ecac73aa45a631e6da34ede
diamant1991 <diamant1988@mail.ru>
2017-12-06 15:00:18 +0200

    add files

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+$('.dropdown-btn').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(":hidden")){
+    $(this).addClass('active')
+    menu.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    menu.slideUp(250)
+  }
+});
+
+$('.partners-carousel').owlCarousel({
+    loop:true,
+    margin: 15,
+    nav:true,
+    dots: false,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      992:{
+          items:4
+      },
+      1200:{
+          items:5
+      }
+    }
+})
